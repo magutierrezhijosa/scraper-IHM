0e8550db3e684cebb09c30307193a2628f270873
Soy Miguel <magutierrezhijosa@gmail.com>
2026-04-03 19:35:48 +0200

    Funcion para recoger la pagina parseada despues de peticion

--- scraper.py
+++ scraper.py
@@ -11,3 +11,32 @@ from config import HEADERS,REQUEST_DELAY, REQUEST_TIMEOUT
 
 # Definimos la funcion que recibe una url que debe ser string
 def fetch_page(url:str) -> BeautifulSoup | None:
+
+    # Iniciamos un try para que si falla el codigo vaya al except
+    try:
+        # Realizamos la peticion de HTTP real 
+        response  = requests.get(url, headers=HEADERS, timeout=REQUEST_TIMEOUT)
+
+        # Comprobamos si el servidor respondio y si hay unn error lo manda al except
+        response.raise_for_status()
+
+        # Pasamos a BeautifulSoup el response.txt y lo parseamos para interpretar ese HTML
+        return BeautifulSoup(response.txt, "html.parser")
+
+    # EXCEPCIONES
+
+    # Si el servidor tardo mas de 15 segundos en responder
+    except requests.exceptions.Timeout:
+        # Mostramos un aviso y devolvemos None
+        print(f"  [!] Timeout al obtener: {url}")
+        return None
+    
+    # Si el servidor respondio con un error HTTP 
+    except requests.exceptions.HTTPError as e:
+        print(f"  [!] Error HTTP {e.response.status_code} en: {url}")
+        return None
+    
+    # Capturamos cualquier otro error que no hayamos previsto antes
+    except requests.exceptions.RequestException as e : 
+        print(f"  [!] Error de conexión en {url}: {e}")
+        return None
