c5755ba342a50cd5c803b122498d96621fe694c7
Soy Miguel <magutierrezhijosa@gmail.com>
2026-04-03 18:41:17 +0200

    Importaciones para el funcionamiento de scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,13 @@
+# Importamos libreria de Python para manejar tiempos
+import time
+# Libreria que importamos parta hacer peticiones HTTP
+import requests
+# Libreria para parsear el HTML que nos devuelva el requests
+from bs4 import BeautifulSoup
+# Importamos CONSTANTES del confi..py
+from config import HEADERS,REQUEST_DELAY, REQUEST_TIMEOUT
 
 
-def fetch_page(url:str) 
+
+# Definimos la funcion que recibe una url que debe ser string
+def fetch_page(url:str) -> BeautifulSoup | None:
