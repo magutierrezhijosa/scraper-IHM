9ba23c2b86350e08dc13267db81b2223e96fccc3
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-09 20:39:14 +0200

    Arreglando problema de los espacios en la URL

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import csv
 # Importamos la funciones :
 # 1."urljoin" para combinar URLs (Convierte enlaces relativos en  absolutos)
 # 2. "urlparse" funcion para desmontar un URL en sus partes:  dominio,ruta,protocolo
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin, urlparse , quote
 
 # Importamos la constantes del archivo config.py
 from config import OUTPUT_CSV,CSV_FIELDNAMES
@@ -12,9 +12,15 @@ from config import OUTPUT_CSV,CSV_FIELDNAMES
 # Declaramos la funcion que se va a encargar de asegurarnos que todos lo enlaces sean absolutos 
 # 1. Si el enlace es absoluto -> lo devuelve tal cual
 # 2. Si el enlace es relativo -> lo combina con la base_url para construir la URL completa
-def  build_absolute_url(href: str, base_url: str) -> str:
-    return urljoin(base_url,href)
-
+def build_absolute_url(href: str, base_url: str) -> str:
+    # Construye la URL absoluta combinando el href con la base_url utilizando urljoin
+    absolute = urljoin(base_url, href)
+    # Para manejar caracteres especiales en la URL, desmontamos la URL y codificamos solo la parte de la ruta
+    parsed = urlparse(absolute)
+    # Codifica la parte de la ruta de la URL para manejar caracteres especiales, dejando los caracteres "/" sin codificar
+    encoded_path = quote(parsed.path, safe="/")
+    # Reconstruye la URL con la ruta codificada y devuelve la URL absoluta final
+    return parsed._replace(path=encoded_path).geturl()
 
 # Declaramos la funcion que se  encarga de desmontar la URL  y nos quedamos solo con la ruta 
 def is_direct_pdf(url: str) -> bool:
