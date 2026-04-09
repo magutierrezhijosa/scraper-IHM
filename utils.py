45369f8d5c7698c8b0d703354e4ff61729b73aca
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-09 18:53:33 +0200

    utils.py terminado

--- utils.py
+++ utils.py
@@ -27,3 +27,20 @@ def is_external_link(url: str, external_domains: list) -> bool:
     hostname = urlparse(url).hostname or ""
     # Compara el dominio extraido con la lista de dominios externos permitidos
     return any(domain in hostname for domain in external_domains)
+
+# Declaramos la funcion que se encarga de guardar los datos extraidos en un archivo CSV
+# Recibe una lista de diccionarios (cada diccionario sera una fila en el CSV con las claves titulo, pdf_url)
+# Recibe el nombre del fichero CSV donde se guardaran los datos
+def save_to_csv(records: list[dict], filename: str = OUTPUT_CSV) -> None:
+    # Abre el archivo CSV en modo escritura y crea un objeto writer para escribir en el archivo
+    with open(filename, mode = 'w', newline='', encoding='utf-8') as f:
+        # Escritor de CSV que utiliza las claves del diccionario como nombres de columna en el archivo CSV
+        writer = csv.DictWriter(f, fieldnames=CSV_FIELDNAMES)
+        # Escribe la fila de encabezado en el archivo CSV
+        writer.writeheader()
+        # Escribe cada registro (diccionario) como una fila en el archivo CSV
+        writer.writerows(records)
+    print(f"\n[✓] Guardados {len(records)} registros en '{filename}'")
+
+    
+    
